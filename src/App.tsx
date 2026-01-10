e82a3f72eb26c01a25feecec91c11ee2d27047f1
IISweetHeartII <sachi009955@gmail.com>
2026-01-11 02:59:01 +0900

    [#22] feat : ToastContainer 적용 및 fade-in 애니메이션 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,23 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import AuthCallback from "./pages/LoginPage/AuthCallback";
 import LoginPage from "./pages/LoginPage/LoginPage";
-import Layout from "../src/components/Layout/Layout";
+import Layout from "./components/Layout/Layout";
 import Survey from "./pages/PickPage/survey";
+import { ToastContainer } from "./components/common/Toast";
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/pick/" element={<Survey />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/auth/callback" element={<AuthCallback />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/pick/" element={<Survey />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/auth/callback" element={<AuthCallback />} />
+        </Route>
+      </Routes>
+      <ToastContainer />
+    </>
   );
 }
 
