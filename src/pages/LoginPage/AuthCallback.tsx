2544c559d7f71e5046f87621ba5a41dbe9f034cd
김덕환 <sachi009955@gmail.com>
2026-01-11 05:02:18 +0900

    [#28] feat : 로그인 페이지 UI 구현 (#29)
    * [#28] feat : 로그인 페이지 UI 구현
    
    * [#28] feat : 디자인 시스템 적용 및 로그인 후 사용자 정보 조회 기능 추가

--- src/pages/LoginPage/AuthCallback.tsx
+++ src/pages/LoginPage/AuthCallback.tsx
@@ -1,16 +1,14 @@
+import { Spinner } from "../../components/common/Spinner";
 import { useAuthRedirect } from "../../hooks/useAuthRedirect";
 
 const AuthCallback = () => {
-  // ✅ 훅 실행! (여기서 토큰 저장하고 알아서 홈으로 튕겨줌)
   useAuthRedirect();
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
-      <h2 className="mt-4 text-xl font-semibold text-gray-700">
-        로그인 처리 중...
-      </h2>
-      <p className="text-gray-500">잠시만 기다려주세요.</p>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-main">
+      <Spinner size="lg" />
+      <h2 className="text-h3 mt-4 text-gray-7">로그인 처리 중...</h2>
+      <p className="text-caption1 text-gray-5">잠시만 기다려주세요.</p>
     </div>
   );
 };
